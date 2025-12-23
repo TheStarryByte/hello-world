5345e7c2c69931b5161921c97fffc47ce3b71ae4
Safa <safamohammed23@hotmail.com>
2025-12-23 09:29:43 +0400

    Add files via upload

---
+++ CS50-2024/C/hello.c
@@ -0,0 +1,10 @@
+// simple program hello world get_string and printf with %s
+
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    string answer = get_string("What's your name? ");
+    printf("hello, %s\n", answer);
+}
